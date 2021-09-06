74e3a93681a2c9c58ee726d761929d5c1a1d0c60
Diego Maeoka <diego.maeoka@infomentum.co.uk>
2021-09-06 16:52:17 +0100

    Move to Alpine

--- .config/postcss.config.js
+++ .config/postcss.config.js
@@ -1,14 +1,16 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss')(path.resolve(__dirname, '../src/tailwind.config.js')),
-    require('autoprefixer'),
-    /**
-     * use tomorrow’s CSS today
-     * docs: https://preset-env.cssdb.org/features
-     */
-    require('postcss-preset-env')({ stage: 1 })
-  ]
-}
+	plugins: [
+		require("postcss-import"),
+		require("tailwindcss")(
+			path.resolve(__dirname, "../src/tailwind.config.js")
+		),
+		require("autoprefixer"),
+		/**
+		 * use tomorrow’s CSS today
+		 * docs: https://preset-env.cssdb.org/features
+		 */
+		require("postcss-preset-env")({ stage: 1 }),
+	],
+};
