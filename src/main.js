fa7be532f405a7dc669e08e8e435fe77fa42f88e
Diego Maeoka <diego.maeoka@infomentum.co.uk>
2021-10-16 00:15:09 +0100

    feat: add x-data

--- src/main.js
+++ src/main.js
@@ -4,8 +4,28 @@
 import "./css/main.scss";
 import Alpine from "alpinejs";
 
+Alpine.data("header", () => ({
+	open: false,
+
+	init() {
+		console.log("Alpine data");
+	},
+	toggle() {
+		this.open = !this.open;
+	},
+}));
+
+Alpine.data("banner", () => ({
+	init() {
+		console.log("Banner");
+	},
+}));
+
+Alpine.data("banner", () => ({
+	init() {
+		console.log("Banner");
+	},
+}));
+
 window.Alpine = Alpine;
-
 Alpine.start();
-
-console.log('test');
